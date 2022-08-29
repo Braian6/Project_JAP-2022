289d08821376d5bdbda282bfe8c2e06f2c69e968
Braian6 <110791517+Braian6@users.noreply.github.com>
2022-08-28 23:16:14 -0300

    Se añade login y requerimiento del mismo para ver pagina principal

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("autos").addEventListener("click", function () {
+    localStorage.setItem("catID", 101);
+    window.location = "products.html";
+  });
+  document.getElementById("juguetes").addEventListener("click", function () {
+    localStorage.setItem("catID", 102);
+    window.location = "products.html";
+  });
+  document.getElementById("muebles").addEventListener("click", function () {
+    localStorage.setItem("catID", 103);
+    window.location = "products.html";
+  });
+  let usuario = JSON.parse(localStorage.getItem("item"));
+  if (usuario == null) {
+    alert("Debe Iniciar sesion");
+    location.href = "login.html";
+  }
+  document.getElementById("chau").addEventListener("click", () => {
+    localStorage.removeItem("item");
+    alert("Gracias por visitar nuestro sitio!! HASTA PRONTO!");
+    location.href = "login.html";
+  });
 });
